f11e32dfcb6df3d628cf9898ab38a01cf9393c23
Tay Clarke <tavia1120@yahoo.com>
2015-01-27 18:51:19 -0500

    Added Bio JSON to Resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,16 @@
+var bio = {
+	"name" : "Tay Clarke",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "804-274-8182",
+		"email": "Tay1120@gmail.com",
+		"github" : "Tay1120",
+		"twitter" : "@Tavia1120",
+		"location" : "Richmond, VA"
+	},
+	"welcomeMessage" : "I am an entry level Web Developer currently in training.",
+	"skills" : [
+	"HTML", "JavaScript","CSS","GitBash and GitHub"
+	],
+	"bioPic":"images/fry.jpg"
+}
