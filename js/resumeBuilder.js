da94eb668b1c58311b3c03d4bb5937c9c6ada4f6
Tay Clarke <tavia1120@yahoo.com>
2015-01-31 18:55:49 -0500

    Added Bio Header and Work Experience to Resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,7 +13,7 @@ var bio = {
 	"HTML", "JavaScript","CSS","GitBash and GitHub"
 	],
 	"bioPic":"images/fry.jpg"
-}
+};
 
 var education = {
 	"schools": [
@@ -43,13 +43,13 @@ var work = {
 			"employer": "Capital One",
 			"title": "Senior Operations Coordinator",
 			"dates": "August 2011-Present",
-			"description": "Fraud Claims Intake, Dispute Claims Intake, Secured Messaging Bank Claims, Payment Investigations, Payment Research, Payment Refunds, Credit Bureau Disputes, Account Research, Adjustments on Credit Card Accounts, Credit Bureau Disputes, Writting and Sending Customer Letters, Training"
+			"description": "Fraud Claims Intake, Dispute Claims Intake, Secured Messaging Bank Claims, Payment Investigations, Payment Research, Payment Refunds, Credit Bureau Disputes, Account Research, Adjustments on Credit Card Accounts, Credit Bureau Disputes, Writting and Sending Customer Letters, Training",
 		},
 		{
 			"employer": "Convergys",
 			"title": "Customer Service and Resolutions Representative",
 			"dates": "October 2010-August 2011",
-			"description": "Customer Service, Manage incoming and outgoing calls, Resolve technical issues with mobile phones, Manage Customer Accounts, Add/Change account features, Equipment and Service Sales"
+			"description": "Customer Service, Manage incoming and outgoing calls, Resolve technical issues with mobile phones, Manage Customer Accounts, Add/Change account features, Equipment and Service Sales",
 		}
 	]
 }
@@ -68,10 +68,47 @@ var project = {
 }
 
 var name = "Tay Clarke"
-var formattedName = HTMLheaderName.replace(
-	"%data%", name);
+var formattedName = HTMLheaderName.replace("%data%", name);
 var role = "Web Developer"
-var formattedRole = HTMLheaderRole.replace(
-	"%data%", role);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#topContacts").append(formattedGithub);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+$("#topContacts").append(formattedTwitter);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#topContacts").append(formattedLocation);
+var formattedPicture = HTMLbioPic.replace("%data%", bio.bioPic);
+$("#header").append(formattedPicture);
+var formattedwelcomeMessage = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+$("#header").append(formattedwelcomeMessage);
+if (bio.skills) {
+            $("#header").append(HTMLskillsStart);
+            for(skills in bio.skills) {
+                var formattedSkills = HTMLskills.replace("%data%", bio.skills[skills]);
+                $("#header").append(formattedSkills);
+            }
+        }
+function displayWork(){
+	for (job in work.jobs) {
+		// create new div for work experience
+		$("#workExperience").append(HTMLworkStart);
+		// concat employer and title
+		var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
+}
+displayWork();
